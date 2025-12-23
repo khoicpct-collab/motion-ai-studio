a4ce493cdd89281a721747686debb6d67a7714fe
khoicpct-collab <khoicpct@gmail.com>
2025-12-23 20:57:33 +0700

    Update file-utils.js

--- utils/file-utils.js
+++ utils/file-utils.js
@@ -1,1 +1,11 @@
-
+// File utility functions
+const FileUtils = {
+    readFile: function(file) {
+        return new Promise((resolve) => {
+            // Simple file reading logic
+            const reader = new FileReader();
+            reader.onload = (e) => resolve(e.target.result);
+            reader.readAsDataURL(file);
+        });
+    }
+};
